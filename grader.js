dfae132d025d92e1440b59c703f860f6fc452231
Alex Key <alexander.key@gmail.com>
2013-07-11 19:43:54 +0000

    updated grader to be able to use a url async for input

--- grader.js
+++ grader.js
@@ -24,35 +24,38 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkFile = function(file, checksfile) {
+    var $ = cheerio.load(file);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
+};
+
+var checkHtmlFile = function(file, checksFile) {
+    return checkFile(fs.readFileSync(file), checksFile);
+
 };
 
 var clone = function(fn) {
@@ -61,14 +64,32 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+var output = function(checkJson) {
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+};
+
+if(require.main == module) {
+    program
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_to_file>', 'Url to an html file')
+	.parse(process.argv);
+
+    if(program.url) {
+	rest.get(program.url).on('complete', function(result) {
+	if(result instanceof Error) {
+	console.error(result);
+	} else {
+	var checkJson = checkFile(result, program.checks);
+	output(checkJson);
+	}
+	});
+    } else {
+	var file = fs.readFileSync(program.file);
+	var checkJson = checkFile(file, program.checks);
+	output(checkJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
